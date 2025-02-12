0289b350ada13957d6fee74f40f2097412209d1d
Iceking <okehchimaobi@gmail.com>
2025-02-12 16:45:24 +0100

    added fullpage scroll on pc screen and above

--- src/components/pages/Homepage.jsx
+++ src/components/pages/Homepage.jsx
@@ -5,14 +5,19 @@ import Projects from '../Projects'
 import Stack from '../Stack'
 import HomepageContact from '../HomepageContact'
 import Footer from '../Footer'
+import { useMediaQuery } from "react-responsive"; //to conditional render the Fullpage slide for large screens
 import Fullpage, { FullpageSection, FullPageSections, FullpageNavigation } from "@ap.cx/react-fullpage";
 
 
 
 
 const Mainpage = () => {
-  return (
+
+  const isLargeScreen = useMediaQuery({minWidth: 1024})
+
+  return isLargeScreen ? (
     <Fullpage className="border">
+      <FullpageNavigation /> {/* adds the navigation clicks on the side of the large screen */}
       <FullPageSections>
         <FullpageSection>
           <Header />
@@ -37,7 +42,14 @@ const Mainpage = () => {
         </FullpageSection>
       </FullPageSections>
     </Fullpage>
-  )
+  ) : <>
+  <Header/>
+    <Hero/>
+    <Projects />
+    <Stack />
+    <HomepageContact />
+    <Footer />
+  </>
 }
 
 export default Mainpage
