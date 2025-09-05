6b6384b9b008108bb007fe283d8027770f798f44
Iceking <okehchimaobi@gmail.com>
2025-09-05 01:14:59 +0100

    added loading spinner and lazy load

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
-import Homepage from "./components/pages/Homepage";
-import AboutPage from "./components/pages/AboutPage";
-import ResumePage from "./components/pages/ResumePage";
 import ScrollToTop from "./components/ScrollToTop";
-import ContactPage from "./components/pages/ContactPage";
-import MoreProjectsPage from "./components/pages/MoreProjectsPage";
+import LoadingSpinner from "./components/LoadingSpinner";
+
+// Lazy load all page components
+const Homepage = lazy(() => import("./components/pages/Homepage"));
+const AboutPage = lazy(() => import("./components/pages/AboutPage"));
+const ResumePage = lazy(() => import("./components/pages/ResumePage"));
+const ContactPage = lazy(() => import("./components/pages/ContactPage"));
+const MoreProjectsPage = lazy(() => import("./components/pages/MoreProjectsPage"));
 
 //05-04-2025
 // tODO - Remove the default drap animation when you scroll to the start and end of the pages in mobile screen
@@ -14,13 +17,15 @@ const App = () => {
   return (
     <div className='overflow-hidden bg-green-400'>
       <ScrollToTop />
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/resume" element={<ResumePage />} />
-        <Route path="/contact" element={<ContactPage />} />
-        <Route path="/more-projects" element={<MoreProjectsPage />} />
-      </Routes>
+      <Suspense fallback={<LoadingSpinner />}>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/resume" element={<ResumePage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/more-projects" element={<MoreProjectsPage />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
